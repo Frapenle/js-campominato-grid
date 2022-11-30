230f5cf9004f991ca8c4af45e4453922f8f007be
francescotrudu <francescotrudu84@gmail.com>
2022-11-30 16:06:44 +0100

    Create element square and add it to html

--- script/script.js
+++ script/script.js
@@ -3,3 +3,10 @@
 for (let i = 1; i <= 100; i++) {
     console.log(i)
 }
+
+// creare elemento square
+const square = document.createElement("div");
+
+// aggiungere elemento square al parent miefield
+const minefield = document.getElementById("minefield")
+minefield.innerHTML = `<div class="square">sss</div>`
