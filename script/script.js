4356fe88f66b5ce8133347268f34a87e0e5ccf18
francescotrudu <francescotrudu84@gmail.com>
2022-11-30 16:45:18 +0100

    Add square numbers

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 // Creare un contatore da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    //aggiungo tanti quadrati al minefield
     // creare elemento square
+    //aggiungo i quadrati al minefield
     const square = document.createElement("div");
     square.classList.add("square");
     
     // aggiungere elemento square al parent minefield
     const minefield = document.getElementById("minefield");
-    minefield.append(square)
+    minefield.append(square);
+    square.innerText = ([i])
 }
